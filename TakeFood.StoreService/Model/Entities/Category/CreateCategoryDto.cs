cf50c183b1cc31d2a64de13e6fe6ae683d986e7b
pnquang2405 <pnquang2405@gmail.com>
2022-11-06 12:14:59 +0700

    fix Add Category

---
+++ TakeFood.StoreService/Model/Entities/Category/CreateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TakeFood.StoreService.Model.Entities.Category
+{
+    public class CreateCategoryDto
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+}
